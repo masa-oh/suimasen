a3d358bc36df61e671a95253041c9ee6eee88a93
masa-oh <makun0084pc@gmail.com>
2021-08-01 14:15:38 +0900

    [Add]マイクテスト画面完成

--- app/javascript/pages/mic_test/index.vue
+++ app/javascript/pages/mic_test/index.vue
@@ -1,28 +1,40 @@
 <template>
-  <section class="section">
-    <div class="columns">
-      <div class="column">
-        <meter :value="audio.volume" min="-96" max="0" high="-40"></meter> <span>{{ audio.volume }}</span>
+  <div class="container-fluid text-center">
+    <div class="row">
+      <div class="d-flex flex-column col-lg-8">
+        <div id="volume-meter">
+          <meter :value="audio.volume" min="-96" max="0" high="-40"></meter> <span>{{ audio.volume }}</span>
+        </div>
+        <div id="canvas-container">
+          <canvas id="canvas" ref="canvas" :width="canvas.width" :height="canvas.height"></canvas>
+        </div>
+      </div>
+      <div class="d-flex flex-column justify-content-center align-items-center col-lg-4">
+        <div id="mic_buttons">
+          <button v-if="!audio.running" @click="getUserMedia" class="btn btn--circle"><i class="fas fa-microphone"></i></br>START</button>
+          <button v-if="audio.running" @click="disconnectMedia" class="btn btn--circle"><i class="fas fa-microphone"></i></br>STOP</button>
+        </div>
+        <div class>
+          <input type="checkbox" id="checkbox" v-model="checked">
+          <label for="checkbox">テスト完了！</label>
+        </div>
+        <div>
+          <router-link :to="{ name: 'GameIndex' }">
+            <button class="btn btn-primary" :disabled="!checked">ゲーム開始</button>
+          </router-link>
+        </div>
       </div>
     </div>
-    <div>
-      <canvas ref="canvas" :width="canvas.width" :height="canvas.height"></canvas>
-    </div>
-    <div class="buttons">
-      <button v-if="!audio.running" @click="getUserMedia">音声の取得を開始する</button>
-      <button v-if="audio.running" @click="disconnectMedia">音声の取得を停止する</button>
-    </div>
-  </section>
+  </div>
 </template>
 
 <script>
+import axios from '../../plugins/axios'
+
 export default {
+  name: "MicTestIndex",
   data() {
     return {
-      streamConst: {
-        audio: true, // 録音する場合はaudio: trueとする
-        video: false,
-      },
       audio: {
         ctx: null,
         processor: null,
@@ -30,7 +42,11 @@ export default {
         mic: null,
         running: false,
         volume: -96,
+        spectrums: null,
+        max_volume: null,
+        max_frequency: null,
       },
+      audioData: [],
       canvas: {
         ctx: null,
         width: 1200,
@@ -43,6 +59,7 @@ export default {
         innerHeight: 560, // height - paddingTop  - paddingBottom
         innerBottom: 580, // height - paddingBottom
       },
+      checked: false,
     }
   },
   mounted() {},
@@ -50,10 +67,12 @@ export default {
     getUserMedia() {
       console.log('getUserMedia')
       navigator.mediaDevices
-        .getUserMedia(this.streamConst)
+        .getUserMedia({ audio: true, video: false })
         .then(stream => {
           this.getLocalMediaStream(stream)
           this.audio.running = true
+          // イベントハンドラー
+          this.audio.processor.onaudioprocess = this.handleOnAudioProcess
         }).catch(error => {
           console.error('mediaDevices.getUserMedia() error:', error)
         })
@@ -63,7 +82,7 @@ export default {
       // AudioContextの利用宣言（クロスブラウザー対応）
       let audioContext = window.AudioContext || window.webkitAudioContext
       // audioContextのオブジェクトを作成
-      this.audio.ctx = new audioContext()
+      this.audio.ctx = new audioContext({ sampleRate: 44100 })
       // 音声データを扱うためのオブジェクトを生成
       // BufferSizeはブラウザが自動で選ぶ(256,512,..,8192,16384)
       this.audio.processor = this.audio.ctx.createScriptProcessor()
@@ -75,12 +94,11 @@ export default {
       // それぞれをconnectで接続し、ストリームの音声データを扱えるようにする
       // mic (Input) -> ScriptProcessorNode (Processing) -> AudioDestinationNode (Output)
       //             └-> AnalyserNode (Visualization)
-      this.audio.mic.connect(this.audio.processor)
-      this.audio.processor.connect(this.audio.ctx.destination)
-      this.audio.mic.connect(this.audio.analyser)
-
-      // イベントハンドラー
-      this.audio.processor.onaudioprocess = this.handleOnAudioProcess
+      this.audio.mic
+        .connect(this.audio.processor)
+        .connect(this.audio.ctx.destination)
+      this.audio.mic
+        .connect(this.audio.analyser)
     },
 
     handleOnAudioProcess(event) {
@@ -96,31 +114,34 @@ export default {
       // sum / input.length: inputBufferから取得した音量の平均値を取得
       // 平方根を取ることで input[i] * input[i] の値をもとに戻す
       this.audio.volume = Math.sqrt(sum / input.length)
-      // 少数第3位を四捨五入し表示
+      // dB換算し、少数第3位を四捨五入し表示
       // this.volume = this.volume.toFixed(2)
-      this.audio.volume = 20 * Math.log10(this.audio.volume);
+      this.audio.volume = (20 * Math.log10(this.audio.volume)).toFixed(2);
 
       // 波形描画
       this.canvas.ctx = this.$refs.canvas.getContext('2d');
-      this.audio.analyser.fftSize = 2048;  // The default value
+      this.audio.analyser.fftSize = 4096;  // The default value
       this.audio.analyser.maxDecibels = 0;
-      this.audio.analyser.minDecibels = -70;
-
-      window.setInterval(this.drawCanvas(), 500);
+      this.audio.analyser.minDecibels = -100;
+      this.drawCanvas();
     },
 
     drawCanvas() {
-      let range = this.audio.analyser.maxDecibels - this.audio.analyser.minDecibels;  // 70 dB
+      let range = this.audio.analyser.maxDecibels - this.audio.analyser.minDecibels;
 
       // Frequency resolution
       let fsDivN = this.audio.ctx.sampleRate / this.audio.analyser.fftSize;
 
-      // This value is the number of samples during 500 Hz
-      let n500Hz = Math.floor(500 / fsDivN);
+      // This value is the number of samples during 440 Hz
+      let n440Hz = Math.floor(440 / fsDivN);
 
       // Get data for drawing spectrum (dB)
-      let spectrums = new Float32Array(this.audio.analyser.frequencyBinCount / 4);  // Array size is 256 (FFT sizeの半分 / 4)
-      this.audio.analyser.getFloatFrequencyData(spectrums);
+      this.audio.spectrums = new Float32Array(this.audio.analyser.frequencyBinCount);  // Array size is 1024
+
+      this.audio.max_volume = this.audio.analyser.minDecibels;
+      this.audio.max_frequency = 0;
+
+      this.audio.analyser.getFloatFrequencyData(this.audio.spectrums);
 
       // Clear previous data
       this.canvas.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -128,9 +149,15 @@ export default {
       // Draw spectrum (dB)
       this.canvas.ctx.beginPath();
 
-      for (let i = 0, len = spectrums.length; i < len; i++) {
-        let x = (i / len) * this.canvas.width;
-        let y = (-1 * ((spectrums[i] - this.audio.analyser.maxDecibels) / range)) * this.canvas.height;
+      for (let i = 0, len = this.audio.spectrums.length / 4; i < len; i++) {
+
+        if (this.audio.max_volume < this.audio.spectrums[i]) {
+          this.audio.max_volume = this.audio.spectrums[i];
+          this.audio.max_frequency = i * fsDivN;
+        }
+
+        let x = Math.floor((i / len) * this.canvas.innerWidth) + this.canvas.paddingLeft;
+        let y = (-1 * ((this.audio.spectrums[i] - this.audio.analyser.maxDecibels) / range) * this.canvas.innerHeight) + this.canvas.paddingTop;
 
         if (i === 0) {
           this.canvas.ctx.moveTo(x, y);
@@ -138,9 +165,9 @@ export default {
           this.canvas.ctx.lineTo(x, y);
         }
 
-        // 500 Hz ?
-        if ((i % n500Hz) === 0) {
-          let text = (500 * (i / n500Hz)) + ' Hz';  // index -> frequency
+        // 440 Hz ?
+        if ((i % n440Hz) === 0) {
+          let text = (440 * (i / n440Hz)) + ' Hz';  // index -> frequency
           // Draw grid (X)
           this.canvas.ctx.fillStyle = 'rgba(255, 0, 0, 1.0)';
           this.canvas.ctx.fillRect(x, this.canvas.paddingTop, 1, this.canvas.innerHeight);
@@ -170,7 +197,7 @@ export default {
       }
       
       // for debug
-      console.log(`audioContext.sampleRate: ${this.audio.ctx.sampleRate}, analyserNode.fftSize: ${this.audio.analyser.fftSize}, fsDivN: ${fsDivN},  n500Hz: ${n500Hz}, spectrums[0]: ${spectrums[0]}, analyserNode.frequencybinCount: ${this.audio.analyser.frequencyBinCount}`);
+      console.log(`audioContext.sampleRate: ${this.audio.ctx.sampleRate}, analyserNode.fftSize: ${this.audio.analyser.fftSize}, fsDivN: ${fsDivN},  n440Hz: ${n440Hz}, spectrums.length: ${this.audio.spectrums.length}, analyserNode.frequencybinCount: ${this.audio.analyser.frequencyBinCount}`);
     },
 
     disconnectMedia() {
@@ -182,10 +209,73 @@ export default {
 
       this.audio.processor.onaudioprocess = null
       this.audio.running = false
+      
+      // axios.get('games', )
+      //  .then(res =>)
     }
   },
 }
 </script>
 
 <style scoped>
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+}
+
+html {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+
+body {
+  background-color: #D8E3E7;
+}
+
+#canvas-container{
+    position: relative;
+    height: 0;
+    overflow: hidden;
+    padding-top: 56.25%;
+}
+  #canvas{
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+  }
+
+button.btn--circle {
+  font-size: 1.6rem;
+  width: 140px;
+  height: 140px;
+  padding: 20px 0 0;
+  border-radius: 50%;
+  color: #fff;
+  background: #51c4d3;
+  -webkit-box-shadow: 0 7px 0 rgba(0, 0, 0, 0.25);
+  box-shadow: 0 7px 0 rgba(0, 0, 0, 0.25);
+}
+
+button.btn--circle i {
+  font-size: 150%;
+}
+
+.fa-position-bottom {
+  position: absolute;
+  bottom: 1rem;
+  left: calc(50% - 0.5rem);
+}
+
+button.btn--circle:active {
+  -webkit-transform: translate(0, 4px);
+  transform: translate(0, 4px);
+  color: #fff;
+  -webkit-box-shadow: 0 3px 0 rgba(0, 0, 0, 0.25);
+  box-shadow: 0 3px 0 rgba(0, 0, 0, 0.25);
+}
 </style>
