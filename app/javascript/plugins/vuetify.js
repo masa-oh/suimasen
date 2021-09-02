01e34f9a35157c634af870073676aafd3d378330
masa-oh <makun0084pc@gmail.com>
2021-09-02 16:53:58 +0900

    [Add]yarnで'sass'を導入してビルドエラーを解消し、テーマカラーを適用

--- app/javascript/plugins/vuetify.js
+++ app/javascript/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/lib/util/colors';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify)
 
@@ -8,8 +9,15 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        background : "#D8E3E7"
-      }
+        primary: "#126E82",
+        secondary: "#D8E3E7",
+        //accent: colors.shades.black,
+        //error: colors.red.accent3,
+        background: "#D8E3E7"
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
     }
   }
 })
